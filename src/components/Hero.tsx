479f4c8d9f86fdfdcd996b01938ea0db935888a6
AnishBangotra <abhibangotra76@gmail.com>
2024-02-26 00:45:21 +0530

    project added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import hero1 from "../assets/hero1.png";
+
+const Hero = () => {
+  return (
+    <div>
+        <img src={hero1} className="w-full max-h-[600px] object-cover"/>
+    </div>
+  );
+};
+
+export default Hero;
